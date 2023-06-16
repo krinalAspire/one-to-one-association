7ee03b2b8e4f2ede7ad4f92f634a17442e367013
krinalAspire <krinal.sonara@aspirespftserv.in>
2023-06-16 14:41:02 +0530

    16 june 2023

---
+++ db/association.js
@@ -0,0 +1,40 @@
+const { Sequelize} = require("sequelize");
+const { DataTypes} =Sequelize;
+
+const sequelize= new Sequelize('test','postgres', 'postgres',{
+    dialect:'postgres'
+});
+
+const country= sequelize.define('country', {
+    countryName:{
+        type:DataTypes.STRING,
+        unique:true
+    }
+})
+
+const Capital= sequelize.define('capital',{
+    CapitalName:{
+        type:DataTypes.STRING,
+        unique:true
+    }
+})
+
+country.hasOne(Capital, {foreignKey:"Id"});
+
+let count, cap;
+
+sequelize.sync({alter:true}).then(()=>{
+   return Capital.findOne({where: { CapitalName: "dfsfhg"}});
+}).then((data)=>{
+    cap=data;
+    return country.findOne({where: {countryName :"ftfhjhj"}})
+}).then((data)=>{
+    count=data;
+    count.setCapital(cap);
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+module.exports= { sequelize, Capital, country};
+
---
+++ index.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const app=express();
+const {country, Capital}=require("./db/association");
+
+app.use(express.json());
+
+app.post("/capital",(req,res)=>{
+     try{
+       const { CapitalName }= req.body;
+       const capital=Capital.create({
+         CapitalName
+       })
+       res.send(capital);
+     }catch(e){
+        res.status(404).send(e)
+     }
+})
+
+app.get("/capital",(req,res)=>{
+  const user=Capital.findAll();
+  res.send(user);
+})
+
+app.post("/country", (req,res)=>{
+    try{
+        const { countryName }= req.body;
+        const capital=country.create({
+            countryName
+        })
+        res.send(capital);
+      }catch(e){
+         res.status(404).send(e)
+      }
+})
+
+app.get("/country",(req,res)=>{
+  const user=country.findAll();
+  res.send(user);
+})
+
+app.listen(7171, ()=>{
+    console.log("server running at 7171");
+})
